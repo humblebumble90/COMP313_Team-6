4998f7300f6d225581f027c2991fe6a20d9533d5
Username909 <43253195+Username909@users.noreply.github.com>
2021-02-25 23:00:59 -0500

    Implement a login button
    Implement a login button that addresses the user to an intended page.

--- app/src/main/java/com/example/fat2fit/MainActivity.java
+++ app/src/main/java/com/example/fat2fit/MainActivity.java
@@ -2,7 +2,10 @@ package com.example.fat2fit;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        /*when pressed will move the screen to a new activity */
+        Button launchBtn = (Button) findViewById(R.id.loginBtn);
+        launchBtn.setOnClickListener (new View.OnClickListener(){
+            public void onClick(View v){
+                startActivity(new Intent(MainActivity.this, HomeActivity.class));
+            }
+        });
     }
+
 }
