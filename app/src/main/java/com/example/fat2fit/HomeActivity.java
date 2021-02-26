bfcaf7094880687576aff2833372e9e0f152770a
Username909 <43253195+Username909@users.noreply.github.com>
2021-02-26 00:28:45 -0500

    Worked on UI for Recommend Workouts
    Implement UI for viewing recomended workouts (frontend)

--- app/src/main/java/com/example/fat2fit/HomeActivity.java
+++ app/src/main/java/com/example/fat2fit/HomeActivity.java
@@ -2,13 +2,23 @@ package com.example.fat2fit;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class HomeActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_home);
+        setContentView(R.layout.activity_main);
+        /*when pressed will move the screen to a new activity */
+        Button launchBtn = (Button) findViewById(R.id.loginBtn);
+        launchBtn.setOnClickListener (new View.OnClickListener(){
+            public void onClick(View v){
+                startActivity(new Intent(HomeActivity.this, RecommendedWorkoutActivity.class));
+            }
+        });
     }
 }
