49d6e13e2b6905d4c64f802e16e1810d538a6ac9
TBriggz <skaterbriggz@gmail.com>
2021-03-13 03:44:01 -0500

    Class for password reset

---
+++ app/src/main/java/com/example/fat2fit/ForgotPasswordActivity.java
@@ -0,0 +1,24 @@
+package com.example.fat2fit;
+
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.DialogInterface;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.view.View;
+
+public class ForgotPasswordActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_forgot_password_one);
+        setTitle("Forgot Password");
+
+    }
+
+    public void resetNext(View view){
+        //TODO
+        }
+}
