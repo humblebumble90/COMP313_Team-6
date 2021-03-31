959b2c915fba4a551bceef99fce4e288c57f3145
TBriggz <skaterbriggz@gmail.com>
2021-03-31 03:59:05 -0400

    Activity - removing group member

---
+++ app/src/main/java/com/example/fat2fit/group_member_remove.java
@@ -0,0 +1,14 @@
+package com.example.fat2fit;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class group_member_remove extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_group_member_remove);
+    }
+}
