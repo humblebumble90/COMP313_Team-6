c981924eadcb9e8f3a9e497b33baa14c327fd62d
Andrew Trinidad <atrinid7@my.centennialcollege.ca>
2021-04-11 14:32:00 -0400

    UI for active challenges
    Added UI for active challenges and the redirect for customer rep

---
+++ app/src/main/java/com/example/fat2fit/ViewParticipatedChallengesActivity.java
@@ -0,0 +1,13 @@
+package com.example.fat2fit;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ViewParticipatedChallengesActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_view_participated_challenges);
+    }
+}
