36bbc4860ae7fa0fa84645b40dfd38334295c945
HyungseokLeee <hyngsuki@gmail.com>
2021-04-11 19:10:31 -0400

    Currentlly did

--- app/src/main/java/com/example/fat2fit/ChallengeAdaptor.java
+++ app/src/main/java/com/example/fat2fit/ChallengeAdaptor.java
@@ -5,6 +5,7 @@ import android.content.Intent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListAdapter;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 
--- app/src/main/java/com/example/fat2fit/ViewParticipatedChallengesActivity.java
+++ app/src/main/java/com/example/fat2fit/ViewParticipatedChallengesActivity.java
@@ -2,6 +2,7 @@ package com.example.fat2fit;
 
 import androidx.appcompat.app.AppCompatActivity;
 import java.util.Arrays;
+import java.util.ArrayList;
 
 import android.os.Bundle;
 import android.view.View;
@@ -9,6 +10,7 @@ import android.widget.ListView;
 
 import com.example.fat2fit.api.Fat2FitApi;
 import com.example.fat2fit.models.Participant;
+import com.example.fat2fit.models.Challenge;
 import com.example.fat2fit.ChallengeAdaptor;
 
 import java.lang.reflect.Array;
@@ -31,9 +33,12 @@ public class ViewParticipatedChallengesActivity extends AppCompatActivity {
         api.getMyActiveChallenges(res -> {
                     listView = findViewById(R.id.listChallenges);
                     Participant[] participants = res.getData();
-                    List<Participant> participantsList = Arrays.asList(participants);
-
-                    challengeAdaptor = new ChallengeAdaptor(this,participantsList);
+                    List<Challenge> challengeList = new ArrayList<Challenge>();
+                    for(Participant p : participants)
+                    {
+                        challengeList.add(p.getChallenge());
+                    }
+                    challengeAdaptor = new ChallengeAdaptor(this,challengeList);
                     listView.setAdapter(challengeAdaptor);
 
 
